7b5c74a6d2754b0e20fd59f78ed10e69a8736b07
maoxiaoke <maoxiaoke@outlook.com>
2022-05-31 18:39:28 +0800

    init pdfme successfully

---
+++ index.ts
@@ -0,0 +1,43 @@
+import fs from 'fs';
+import path from 'path';
+import { Template, generate, BLANK_PDF } from '@pdfme/generator';
+
+const template: Template = {
+  basePdf: BLANK_PDF,
+  schemas: [
+    {
+      a: {
+        type: 'text',
+        position: { x: 0, y: 0 },
+        width: 10,
+        height: 10,
+      },
+      b: {
+        type: 'text',
+        position: { x: 10, y: 10 },
+        width: 10,
+        height: 10,
+      },
+      c: {
+        type: 'text',
+        position: { x: 20, y: 20 },
+        width: 10,
+        height: 10,
+      },
+    },
+  ],
+};
+
+const inputs = [{ a: 'a1', b: 'b1', c: 'c1' }];
+
+generate({ template, inputs }).then((pdf) => {
+  console.log(pdf);
+
+  // Browser
+  // const blob = new Blob([pdf.buffer], { type: 'application/pdf' });
+  // window.open(URL.createObjectURL(blob));
+
+  // Node.js
+  fs.writeFileSync(path.join(__dirname, `test.pdf`), pdf);
+});
+
